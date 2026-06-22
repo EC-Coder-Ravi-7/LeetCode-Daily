0f3baa7bd7db153e30c0cdc9befcc84c81ad5301
Ravi Shanakar Gupta <159308710+EC-Coder-Ravi-7@users.noreply.github.com>
2026-06-23 00:14:28 +0530

    Time: 0 ms (100%), Space: 8.9 MB (84.03%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        
+        int b = 0, a = 0, l = 0, o = 0, n = 0;
+        for(auto &x : text){
+            if(x == 'b'){
+                b++;
+            }
+            if(x == 'a'){
+                a++;
+            }
+            if(x == 'l'){
+                l++;
+            }
+            if(x == 'o'){
+                o++;
+            }
+            if(x == 'n'){
+                n++;
+            }
+        }
+        
+        int count = 0;
+        while(b > 0 && a > 0 && l > 1 && o > 1 && n > 0){
+            count++;
+            b-=1, a-=1, l-=2, o-=2, n-=1;
+        }
+
+        return count;
+    }
+};
