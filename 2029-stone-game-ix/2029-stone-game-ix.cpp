5da6eacc621995ec96b143cfc9d3fa9f986b2c53
Ravi Shanakar Gupta <159308710+EC-Coder-Ravi-7@users.noreply.github.com>
2026-08-17 00:25:05 +0530

    Time: 3 ms (75.38%), Space: 131.2 MB (92.89%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for(auto &x : stones){
+            if(x%3 == 0){
+                cnt0++;
+            } else if(x%3 == 1){
+                cnt1++;
+            } else {
+                cnt2++;
+            }
+        }
+        if(cnt0%2 == 0){
+            if(cnt1 > 0 && cnt2 > 0){
+                return true;
+            } else return false;
+        } else {
+            if(abs(cnt1-cnt2) >= 3) return true;
+            else return false;
+        }
+        return false;
+    }
+};
